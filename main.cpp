010e39dffd62c1139ebf99efa8d19493971a732c
matesz1236 <mate.toth1236@gmail.com>
2026-02-26 08:46:43 +0100

    Hibak kijavitva

--- main.cpp
+++ main.cpp
@@ -4,23 +4,37 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //NELEMENTS nincs deklarálva (N_ELEMENTS kellene)
-    std::cout << '1-100 ertekek duplazasa' //szimpla idézőjel (') helyett dupla (") kell, és hiányzik a pontosvessző (;)
-    for (int i = 0;) //hiányzik a ciklus feltétele és a növekmény
+  
+    int *b = new int[N_ELEMENTS]; 
+    
+    std::cout << "1-100 ertekek duplazasa" << std::endl; 
+    
+
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) //a feltétel (i) hibás, így a ciklus sosem fut le (i < N_ELEMENTS kellene)
+    
+
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
-        std::cout << "Ertek:" //hiányzik a pontosvessző (;) és nem írja ki az értéket (b[i])
+        std::cout << "Ertek: " << b[i] << std::endl; 
     }    
+    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; //az atlag változónak nincs kezdőértéke (0 kellene)
-    for (int i = 0; i < N_ELEMENTS, i++) //a vessző (,) helyett pontosvessző (;) kell a ciklusba
+    
+
+    int atlag = 0; 
+    
+
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
-        atlag += b[i] //hiányzik a pontosvessző (;) a sor végéről
-    }
+        atlag += b[i]; 
+    
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    
+    delete[] b; 
+    
     return 0;
 }
